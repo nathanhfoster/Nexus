e91b02b0203aa1df498e3cdebc158ab64b9f637e
strap <nateinthegame@gmail.com>
2017-11-02 10:11:03 -0700

    new images

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $("#project-page").addClass("project-page");
 });
+
+// $(window).scroll(function() {
+//   $(".parallax").css({
+//     'opacity': 1 - (($(this).scrollTop()) / 250)
+//   });
+// });
