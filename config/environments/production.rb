2ef253e79f5c3db6a9eff4dd0f06fdfff3601c98
strap <nateinthegame@gmail.com>
2017-11-02 10:48:19 -0700

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,11 +24,17 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :true
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+  config.serve_static_assets = false
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed  
   config.assets.compile = false
+
+  # Generate digests for assets URLs  
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
